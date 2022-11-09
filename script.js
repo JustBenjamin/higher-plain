76dbc252a491a2928a909f33438a96c3241ea8db
Benjamin Hove <benhove86@gmail.com>
2022-11-09 11:29:19 -0500

    The firts version, v-01

---
+++ script.js
@@ -0,0 +1,28 @@
+var shoppingCart = (function() {
+
+    cart = [];
+
+
+    //constructor
+    function Item(name, price, count) {
+        this.name = name;
+        this.price = price;
+        this.count = count;
+    }
+
+    //save cart 
+    function saveCart() {
+        sessionStorage.setItem('shoppingCart', JSON.stringify(cart));
+    }
+
+    //Load cart 
+    function loadCart() {
+        cart = JSON.parse(sessionStorage.getItem('shoppingCart'))
+        if (sessionStorage.getItem("shoppingCart") != null) {
+            loadCart();
+        }
+    }
+
+
+})
+
