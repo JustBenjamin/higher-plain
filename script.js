703a83a4fa70809e05f3ff115d1b602abf0b5c4d
Benjamin Hove <benhove86@gmail.com>
2022-11-09 12:49:35 -0500

    Added cart methods

--- script.js
+++ script.js
@@ -28,8 +28,60 @@ var shoppingCart = (function() {
     var obj = {};
 
     //adding to cart 
-    
+    obj.addItemToCart = function(name, price, count) {
+        for(var item in cart) {
+            if(cart[item].name === name) {
+                cart[item].count ++;
+                saveCart();
+                return;
+            }
+        }
 
+        var item = new Item(name, price, count);
+        cart.push(item);
+        saveCart();
+    }
+
+    // set count from item
+    obj.setCountForItem = function(name, count) {
+        for(var i in cart) {
+            if (cart[i].name === name) {
+                cart[i].count = count;
+                break;
+            }
+        }
+    };
+
+    //removing item from cart 
+    obj.removeItemFromCart = function(name) {
+        for(var item in cart) {
+            if(cart[item.name === name]) {
+                cart[item].count --;
+                if(cart[item].count === 0) {
+                    cart.splice(item, 1);
+                }
+                break;
+            }
+    }
+    saveCart();
+    }
+
+    //removing all items from cart 
+    obj.removeItemFromCartAll = function(name) {
+        for(var item in cart) {
+            if(cart[item].name === name) {
+                cart.splice(item, 1);
+                break;
+            }
+        }
+        saveCart();
+    }
+
+    // clearing the cart
+    obj.clearCart = function() {
+        cart = [];
+        saveCart();
+    }
 
 })
 
